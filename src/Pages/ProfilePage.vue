4d06a96d2682c68081293fb26c9bd1a370731267
oussama smiai <smiai.oussama@gmail.com>
2021-09-16 17:30:06 +0100

    init instagramy

---
+++ src/Pages/ProfilePage.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <stories-section/>
+    </div>
+</template>
+<script>
+import StoriesSection from '../components/StoriesSection'
+export default{
+    components: {
+        StoriesSection
+    }
+}
+</script>
+<style>
+
+</style>
---
+++ src/components/StoriesSection.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <v-layout>
+            <v-flex>
+                <div class="title">Feature Stories</div>
+            </v-flex>
+        </v-layout>
+    </div>
+</template>
+<script>
+export default{
+    
+        
+}
+</script>
+<style>
+
+</style>
